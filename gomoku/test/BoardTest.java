744b39d0dc30fdca8dc33772a9f7eff73e9aed3c
mcheungsen <maevachgsn@gmail.com>
2023-07-31 22:57:07 +0200

    :white_check_mark: some tests

--- gomoku/test/BoardTest.java
+++ gomoku/test/BoardTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BoardTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+    Board b;
+
+
+    @Before
+    public void initialize(){
+        b = new Board(5,4);
+    }
+
+    @Test
+    public void testAttributes(){
+        Assert.assertEquals(5, b.getRow());
+        Assert.assertEquals(4, b.getCol());
+
+        for(int i = 0; i < b.getRow(); i++){
+            for (int j = 0; j < b.getCol(); j++){
+                Assert.assertEquals(Color.NONE, b.get(new Position(i,j)));
+                b.set(new Position(i,j), Color.WHITE);
+                Assert.assertEquals(Color.WHITE, b.get(new Position(i,j)));
+                b.set(new Position(i,j), Color.BLACK);
+                Assert.assertEquals(Color.BLACK, b.get(new Position(i,j)));
+            }
+        }
+
+
+    }
 }
--- gomoku/test/PositionTest.java
+++ gomoku/test/PositionTest.java
@@ -3,14 +3,31 @@ import Exceptions.InvalidCoordinatesException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+
 
 public class PositionTest {
 
     @Test
-    public void checkPosition() throws InvalidCoordinatesException, GamePlayerLeavesException {
+    public void testValidPositionString() throws InvalidCoordinatesException, GamePlayerLeavesException {
         Position p = new Position("A4");
-        Assert.assertEquals(0,p.row);
-        Assert.assertEquals(3, p.col);
+        Assert.assertEquals(3,p.row);
+        Assert.assertEquals(0, p.col);
+    }
+
+    @Test(expected = GamePlayerLeavesException.class)
+    public void testQuitGame() throws GamePlayerLeavesException, InvalidCoordinatesException {
+        String str = "/quit";
+        Position p = new Position(str);
+    }
+
+    @Test
+    public void testInvalidPosition(){
+        assertThrows(InvalidCoordinatesException.class, () -> new Position("M333"));
+        assertThrows(InvalidCoordinatesException.class, () -> new Position("A12B"));
+        assertThrows(InvalidCoordinatesException.class, () -> new Position("XYZ"));
+        assertThrows(InvalidCoordinatesException.class, () -> new Position("5"));
+        assertThrows(InvalidCoordinatesException.class, () -> new Position("-1"));
     }
 
 }
